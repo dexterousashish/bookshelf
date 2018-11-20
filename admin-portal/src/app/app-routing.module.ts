4f98ea58a1032c98b10599684180b6d6ea16a871
Ashish Singh <dexterousashish@gmail.com>
2018-11-20 23:01:10 +0530

    modified files for imports and dependencies

--- admin-portal/src/app/app-routing.module.ts
+++ admin-portal/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule, MatButtonModule} from '@angular/material';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,ReactiveFormsModule,HttpClientModule,MatNativeDateModule,MatButtonModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
